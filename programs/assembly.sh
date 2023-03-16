d6eb6942082bfb1f4b18d8303cfe73737706b365
runekri3 <kristofer.kirss@hotmail.com>
2023-03-16 23:05:28 +0200

    feat: Only create build dir if it doesn't exist

--- programs/assembly.sh
+++ programs/assembly.sh
@@ -5,6 +5,6 @@ DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
 SRC_DIR="$DIR/src"
 BUILD_DIR="$DIR/build"
 
-mkdir "$BUILD_DIR"
+mkdir -p "$BUILD_DIR"
 
 riscv64-unknown-elf-gcc -S -mbranch-cost=2 -mabi=ilp32 -misa-spec=2.2 -march=rv32i -mstrict-align -o "$BUILD_DIR/fibonacci.s" "$SRC_DIR/fibonacci.c"
--- programs/compile.sh
+++ programs/compile.sh
@@ -5,7 +5,7 @@ DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
 SRC_DIR="$DIR/src"
 BUILD_DIR="$DIR/build"
 
-mkdir "$BUILD_DIR"
+mkdir -p "$BUILD_DIR"
 
 riscv64-unknown-elf-gcc -mabi=ilp32 -misa-spec=2.2 -march=rv32i -mstrict-align -mbranch-cost=2 -s -nostartfiles "$SRC_DIR/fibonacci.c" -o "$BUILD_DIR/fibonacci.elf"
 riscv64-unknown-elf-strip -S --strip-unneeded --remove-section=.note.gnu.gold-version --remove-section=.comment --remove-section=.note --remove-section=.note.gnu.build-id --remove-section=.note.ABI-tag -R .note -R .comment -R .eh_frame -R .eh_frame_hdr "$BUILD_DIR/fibonacci.elf"
