8fead06f5ee4c079c4a6b950120c2477cd0aecb2
runekri3 <kristofer.kirss@hotmail.com>
2023-03-20 04:16:37 +0200

    fix: Fix fibonacci n being off by 1

--- programs/src/fibonacci.c
+++ programs/src/fibonacci.c
@@ -5,7 +5,7 @@ int main() {
     int t2 = 0;
     int nextTerm = 1;
 
-    for (int i = 1; i <= n; ++i) {
+    for (int i = 2; i <= n; ++i) {
         t1 = t2;
         t2 = nextTerm;
         nextTerm = t1 + t2;
