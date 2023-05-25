f757fd4294c5d1885cfd5e1efec074cf647d5879
runekri3 <kristofer.kirss@hotmail.com>
2023-05-25 18:36:27 +0300

    feat: Add fibonacciRepeating.c to run 44 fibonacci iterations 1000 times

---
+++ programs/src/fibonacciRepeating.c
@@ -0,0 +1,22 @@
+int main() {
+    int fibonacciLoops = 1000;
+    int fibonacciTotal = 0;
+
+    for (int fibonacciLoop = 0; fibonacciLoop <= fibonacciLoops; ++fibonacciLoop) {
+        int n = 46;
+
+        int t1 = 0;
+        int t2 = 0;
+        int nextTerm = 1;
+
+        for (int i = 2; i <= n; ++i) {
+            t1 = t2;
+            t2 = nextTerm;
+            nextTerm = t1 + t2;
+        }
+
+        fibonacciTotal = fibonacciTotal + nextTerm;
+    }
+
+    return fibonacciTotal;
+}
