85345377f86730fb00e0fd929d031ecbcf6ae254
kkirss <kristofer.kirss@hotmail.com>
2023-08-12 01:32:46 +0300

    feat: Simplify init assembly

--- programs/init_assemblies/default.s
+++ programs/init_assemblies/default.s
@@ -1,28 +1,14 @@
-# Copied from https:#github.com/ChrisShakkour/RV32I-MAF-project/blob/main/scripts/crt0/crt0.S
-
 .section .init
 .global _start
 .type _start,@function
 
-# init stack pointer
-# jump to call_main_handler
 _start:
+    # Initialize stack pointer to __C_STACK_TOP (defined in the linker script)
 	la sp, __C_STACK_TOP__
-	j call_main_handler
-
-# jump to main program
-# (argc = argv = 0)
-call_main_handler:
-	li x10, 0
-	li x11, 0
+	# Call main() function
 	jal main
-	j end_of_main_handler
-
-# stuck at, indicator of end of execution.
-idle:
+	# Run in infinite loop after main() returns
 	j idle
 
-# move main function status/return value from a0 to saved register s0.
-end_of_main_handler:
-	mv s0, a0
+idle:
 	j idle
