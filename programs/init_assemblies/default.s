9ef925756c0c76b3181571e4e57a31faa45f074f
kkirss <kristofer.kirss@hotmail.com>
2023-08-14 12:12:51 +0300

    fix: Re-add nop instruction to init assembly

--- programs/init_assemblies/default.s
+++ programs/init_assemblies/default.s
@@ -3,6 +3,8 @@
 .type _start,@function
 
 _start:
+    # Start with no-op as VMEM address 0 is not accessible
+    nop
     # Initialize stack pointer to __C_STACK_TOP (defined in the linker script)
 	la sp, __C_STACK_TOP__
 	# Call main() function
