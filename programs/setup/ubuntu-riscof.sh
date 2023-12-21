e138342092de79169fa0c1fa867f9eaff00e5106
kkirss <kristofer.kirss@hotmail.com>
2023-12-21 13:51:53 +0200

    feat: Create link to riscv32-unknown-elf-gcc in riscof setup script

--- programs/setup/ubuntu-riscof.sh
+++ programs/setup/ubuntu-riscof.sh
@@ -7,3 +7,11 @@ else
     # TODO: Docs say Python 3.6 is required but using default Python instead
     pip3 install git+https://github.com/riscv/riscof.git
 fi
+
+if which riscv32-unknown-elf-gcc > /dev/null 2>&1; then
+    echo "riscv32-unknown-elf-gcc is already installed/linked"
+else
+    echo "Creating link to riscv32-unknown-elf-gcc..."
+    echo "(The 64-bit compiler supports 32-bit compilation. But riscof doesn't know that.)"
+    ln -s /opt/riscv/bin/riscv64-unknown-elf-gcc /opt/riscv/bin/riscv32-unknown-elf-gcc
+fi
