e5bb7c11cac0099a283661e60db60cacdefc4ad9
kkirss <kristofer.kirss@hotmail.com>
2023-12-21 12:18:15 +0200

    feat: Get video pointer address from linker script

--- programs/src/display_hello/vdisplay_driver.h
+++ programs/src/display_hello/vdisplay_driver.h
@@ -1,8 +1,8 @@
 
 const unsigned int WORD_SIZE = 32;
 
-// TODO: Make this more dynamic/portable
-const unsigned int FRAME_BUFFER_POINTER_ADDR = 0x200000 * (8 / WORD_SIZE);
+extern unsigned int __DISPLAY_POINTER_ADDRESS__;
+const unsigned int FRAME_BUFFER_POINTER_ADDR = __DISPLAY_POINTER_ADDRESS__;
 
 void configure_display(unsigned int x, unsigned int y, unsigned int color_depth);
 unsigned int* allocate_frame_buffer();
