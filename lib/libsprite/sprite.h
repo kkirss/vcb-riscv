32b130e9571db733b201ddcacfc441c51fe7776c
kkirss <kristofer.kirss@hotmail.com>
2023-12-25 14:34:40 +0200

    feat: Add IsSprite concept to simplify template definitions

--- lib/libsprite/sprite.h
+++ lib/libsprite/sprite.h
@@ -1,8 +1,19 @@
 #pragma once
 
-template<typename T, unsigned int WIDTH, unsigned int HEIGHT>
+#include <type_traits>
+
+template<typename PIXEL_T, unsigned int WIDTH, unsigned int HEIGHT>
 struct Sprite {
-    T pixels[HEIGHT][WIDTH];
+    PIXEL_T pixels[HEIGHT][WIDTH];
     static const unsigned int width = WIDTH;
     static const unsigned int height = HEIGHT;
 };
+
+template<typename T>
+struct is_sprite : std::false_type {};
+
+template<typename PIXEL_T, unsigned int WIDTH, unsigned int HEIGHT>
+struct is_sprite<Sprite<PIXEL_T, WIDTH, HEIGHT>> : std::true_type {};
+
+template<typename T>
+concept IsSprite = is_sprite<T>::value;
--- lib/libvdisplay_driver/vdisplay_driver.h
+++ lib/libvdisplay_driver/vdisplay_driver.h
@@ -40,10 +40,10 @@ public:
         }
     }
 
-    template<typename SPRITE_T, unsigned int SPRITE_WIDTH, unsigned int SPRITE_HEIGHT>
-    static void draw_sprite(const unsigned int x_pos, const unsigned int y_pos, const Sprite<SPRITE_T, SPRITE_WIDTH, SPRITE_HEIGHT>& sprite, unsigned int *frame_buffer) {
-        for (unsigned int y = 0; y < SPRITE_HEIGHT; ++y) {
-            for (unsigned int x = 0; x < SPRITE_WIDTH; ++x) {
+    template<IsSprite SpriteType>
+    static void draw_sprite(const unsigned int x_pos, const unsigned int y_pos, const SpriteType& sprite, unsigned int *frame_buffer) {
+        for (unsigned int y = 0; y < SpriteType::height; ++y) {
+            for (unsigned int x = 0; x < SpriteType::width; ++x) {
                 set_pixel_1b(x_pos + x, y_pos + y, sprite.pixels[y][x], frame_buffer);
             }
         }
