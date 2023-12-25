ee5c1616c97ec180d55e5d7f1f92feb8f6c4fc64
kkirss <kristofer.kirss@hotmail.com>
2023-12-25 16:08:37 +0200

    feat: Calculate pixel address and mask based on color depth

--- lib/libvdisplay_driver/vdisplay_driver.h
+++ lib/libvdisplay_driver/vdisplay_driver.h
@@ -16,6 +16,9 @@ public:
     static constexpr unsigned int PIXELS_PER_WORD = WORD_SIZE / COLOR_DEPTH;
     static_assert(WIDTH % PIXELS_PER_WORD == 0, "Display width must be a multiple of the number of pixels per word");
 
+    static constexpr unsigned int PIXEL_MASK_LSB = (1 << COLOR_DEPTH) - 1;
+    static constexpr unsigned int PIXEL_MASK_MSB = PIXEL_MASK_LSB << (WORD_SIZE - COLOR_DEPTH);
+
     static constexpr unsigned int FRAME_BUFFER_SIZE = (WIDTH * HEIGHT * COLOR_DEPTH) / WORD_SIZE;
     static inline unsigned int frame_buffer_a[FRAME_BUFFER_SIZE];
     static inline unsigned int frame_buffer_b[FRAME_BUFFER_SIZE]; // For future use
@@ -25,13 +28,16 @@ public:
         const unsigned int sequential_pixel_number = x + (y * WIDTH);
 
         // Calculate the word address within the frame buffer
-        const unsigned int mem_word_address = sequential_pixel_number / WORD_SIZE;
+        const unsigned int mem_word_address = sequential_pixel_number / PIXELS_PER_WORD;
 
         // Get reference to the word where the pixel is located
         unsigned int &pixel_word = frame_buffer[mem_word_address];
 
-        // Calculate the bit mask for the pixel, within the word
-        const unsigned int mem_pixel_mask = 1 << (WORD_SIZE - (sequential_pixel_number % WORD_SIZE) - 1);
+        // Calculate the index of the pixel within the word
+        const unsigned int pixel_word_index = sequential_pixel_number % PIXELS_PER_WORD;
+
+        // Calculate the mask for the pixel within the word (MSB is left-most pixel)
+        const unsigned int mem_pixel_mask = PIXEL_MASK_MSB >> pixel_word_index;
 
         // Set or clear the pixel based on new_color
         if (new_color == 1) {
