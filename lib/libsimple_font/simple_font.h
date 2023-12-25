14dd5fa2b688b3578f67dc5e06ff6e0dad422a7e
kkirss <kristofer.kirss@hotmail.com>
2023-12-25 17:04:09 +0200

    feat: Add more characters to libsimple_font

--- lib/libsimple_font/simple_font.h
+++ lib/libsimple_font/simple_font.h
@@ -5,6 +5,14 @@
 namespace SimpleFont {
     typedef Sprite<unsigned int, 4, 5> SpriteType;
 
+    extern const SpriteType D;
+    extern const SpriteType E;
     extern const SpriteType H;
     extern const SpriteType I;
+    extern const SpriteType L;
+    extern const SpriteType O;
+    extern const SpriteType R;
+    extern const SpriteType W;
+    extern const SpriteType SPACE;
+    extern const SpriteType EXCLAMATION_MARK;
 };
