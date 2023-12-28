50e88a223d22ffbf3bcb54408f521683e8c86054
kkirss <kristofer.kirss@hotmail.com>
2023-12-28 04:45:01 +0200

    feat: Use concrete Sprite type in draw_sprite signature

--- lib/libvdisplay_sprite/vdisplay_sprite.h
+++ lib/libvdisplay_sprite/vdisplay_sprite.h
@@ -3,13 +3,13 @@
 #include "sprite.h"
 #include "vdisplay_driver.h"
 
-template <DisplayConfig display_config, IsSprite SpriteType>
+template <DisplayConfig display_config, unsigned int SPRITE_WIDTH, unsigned int SPRITE_HEIGHT>
 static void draw_sprite(const unsigned int x_pos,
                         const unsigned int y_pos,
-                        const SpriteType &sprite,
+                        const Sprite<SPRITE_WIDTH, SPRITE_HEIGHT> &sprite,
                         FrameBuffer &frame_buffer) {
-    for (unsigned int y = 0; y < SpriteType::height; ++y) {
-        for (unsigned int x = 0; x < SpriteType::width; ++x) {
+    for (unsigned int y = 0; y < SPRITE_HEIGHT; ++y) {
+        for (unsigned int x = 0; x < SPRITE_WIDTH; ++x) {
             draw_pixel<display_config>(x_pos + x, y_pos + y, sprite.pixels[y][x], frame_buffer);
         }
     }
