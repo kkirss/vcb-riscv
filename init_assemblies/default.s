5648d74cfc9e0ff1f7f5570fe506f83400f78924
kkirss <kristofer.kirss@hotmail.com>
2023-12-29 05:37:37 +0200

    refactor: Minor format changes to init assembly

--- init_assemblies/default.s
+++ init_assemblies/default.s
@@ -3,12 +3,15 @@
 .type _start,@function
 
 _start:
-    # Start with no-op as VMEM address 0 is not accessible
-    nop
-    # Initialize stack pointer to __C_STACK_TOP (defined in the linker script)
+  # Start with no-op as VMEM address 0 is not accessible
+  nop
+
+  # Initialize stack pointer to __C_STACK_TOP (defined in the linker script)
 	la sp, __C_STACK_TOP__
+
 	# Call main() function
 	jal main
+
 	# Run in infinite loop after main() returns
 	j idle
 
